705d45861dff67e0d83f5fa01b1e4bf904d4b579
GN0013 <geovanni.nicolosi000@gmail.com>
2023-10-19 01:42:32 -0300

    nova-correção

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        // Dados 
-        String agencia = "067-8";
-        int numero = 1021;
-        String nomeCliente = "Mario Andrade";
-        double saldo = 237.48;
+        Scanner scanner = new Scanner(System.in);
 
-        // Sysout
-        System.out.println("Agência: " + agencia);
-        System.out.println("Conta: " + numero);
-        System.out.println("Nome do Cliente: " + nomeCliente);
-        System.out.println("Saldo: " + saldo);
+        System.out.println("Por favor, insira os seguintes dados:");
 
-        // Mensagem
+        System.out.print("Número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); 
+
+        System.out.print("Nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Saldo da Conta: ");
+        double saldo = scanner.nextDouble();
+
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        
+        scanner.close();
     }
 }
