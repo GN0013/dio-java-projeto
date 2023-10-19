039d2e6427b757dbdd453727a9f8feda82713210
GN0013 <geovanni.nicolosi000@gmail.com>
2023-10-19 01:08:02 -0300

    final-projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,18 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Dados 
+        String agencia = "067-8";
+        int numero = 1021;
+        String nomeCliente = "Mario Andrade";
+        double saldo = 237.48;
+
+        // Sysout
+        System.out.println("Agência: " + agencia);
+        System.out.println("Conta: " + numero);
+        System.out.println("Nome do Cliente: " + nomeCliente);
+        System.out.println("Saldo: " + saldo);
+
+        // Mensagem
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
